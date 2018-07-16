f0126d7dc120412a832127f0c5e616ba9a2509ca
Phil de Joux <philderbeast@gmail.com>
2018-07-16 07:41:45 -0400

    Include the simplified generated code in readme and code.

--- zone/library/Flight/Zone.hs
+++ zone/library/Flight/Zone.hs
@@ -23,3 +23,63 @@ data Zone k a where
 
 deriving instance Eq a => Eq (Zone k a)
 deriving instance (Eq a, Ord a) => Ord (Zone k a)
+
+-- NOTE: Below is the generated code from the dump with the following
+-- replacements made.
+-- :%s/GHC\.Classes\.//
+-- :%s/GHC\.Types\.//
+-- :%s/Flight\.Zone\.//
+{-
+instance (Eq a, Ord a) =>
+           Ord (Zone k a) where
+    compare a_a2hw b_a2hx
+      = case a_a2hw of
+          Point
+            -> case b_a2hx of
+                 Point -> EQ
+                 _ -> LT
+          Vector
+            -> case b_a2hx of
+                 Point {} -> GT
+                 Vector -> EQ
+                 _ -> LT
+          Conical a1_a2hy
+            -> case b_a2hx of
+                 Conical b1_a2hz
+                   -> (a1_a2hy `compare` b1_a2hz)
+                 _ -> GT
+    (<) a_a2hA b_a2hB
+      = case a_a2hA of
+          Point
+            -> case b_a2hB of
+                 Point -> False
+                 _ -> True
+          Vector
+            -> case b_a2hB of
+                 Point {} -> False
+                 Vector -> False
+                 _ -> True
+          Conical a1_a2hC
+            -> case b_a2hB of
+                 Conical b1_a2hD -> (a1_a2hC < b1_a2hD)
+                 _ -> False
+    (<=) a_a2hE b_a2hF
+      = not ((<) b_a2hF a_a2hE)
+    (>) a_a2hG b_a2hH = (<) b_a2hH a_a2hG
+    (>=) a_a2hI b_a2hJ
+      = not ((<) a_a2hI b_a2hJ)
+  
+instance Eq a =>
+           Eq (Zone k a) where
+    (==) (Point) (Point)
+      = True
+    (==) (Vector) (Vector)
+      = True
+    (==)
+      (Conical a1_a2hK)
+      (Conical b1_a2hL)
+      = ((a1_a2hK == b1_a2hL))
+    (==) _ _ = False
+    (/=) a_a2hM b_a2hN
+      = not ((==) a_a2hM b_a2hN)
+-}
